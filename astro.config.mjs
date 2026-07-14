3900e8d6f59a52bfc1bda0001df868ea3da89b1a
Jeronit1 <jeropourtauf@gmail.com>
2026-07-14 15:21:40 -0300

    Islas en astro con preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://proyectoastro.netlify.app"
+  site: "https://proyectoastro.netlify.app",
+  integrations: [preact()]
 });
